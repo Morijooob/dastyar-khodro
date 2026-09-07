2ae46aad4dd4427e1b5d8531ea1cc53f316ba322
Morijooob <124460920+Morijooob@users.noreply.github.com>
2026-09-07 07:29:30 +0330

    fix: use valid Capacitor web directory

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'ir.dastyarkhodro.app',
   appName: 'دستیار خودرو',
-  webDir: '.',
+  webDir: 'www',
   bundledWebRuntime: false,
   server: {
     androidScheme: 'https'
